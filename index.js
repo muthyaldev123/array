8afeec61fd423ec7fbeedf8c6eaa6ba67aa355de
muthyaldev123 <pulikantimuthyalreddy@gmail.com>
2024-08-23 22:00:05 +0530

    array

---
+++ index.js
@@ -0,0 +1,26 @@
+// let arr=["hello","word","this","is","javascript"]
+// console.log(arr.join(' '));
+
+
+// let arr=[1,2,3,4,5,]
+// delete arr[2];
+// console.log(arr)
+
+// let arr1=[1,2,3]
+// let arr2=[4,5,6]
+// res=arr1.concat(arr2);
+// console.log(res)
+
+// let arr=[[1,2],[3,4],[5,[6,7]]]
+// res=arr.flat(3);
+// console.log(res);
+
+// let arr=["apple","banana","orange","grape"]
+//  arr.splice(1,1,"kiwi","mango");
+// console.log(arr);
+
+// let arr=[1,2,3,4,5,6,7,8]
+// let x=arr.slice(2,5)
+// console.log(x);
+
+
